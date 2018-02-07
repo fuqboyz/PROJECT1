b6272382eaa135a31ac0472d6ab8150fd495abb0
mikey <michael.w@shaw.ca>
2018-02-07 14:43:29 -0800

    total project added

---
+++ __a51.bat
@@ -0,0 +1,5 @@
+@echo off
+C:
+cd "\Users\Michael\Desktop\ECE\CrossIDE\CrossIDE\Call51\Bin\"
+"C:\Users\Michael\Desktop\ECE\CrossIDE\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\Michael\Desktop\ECE\CrossIDE\CrossIDE\Call51\Bin\project1.asm"
+echo Crosside_Action Set_Hex_File C:\Users\Michael\Desktop\ECE\CrossIDE\CrossIDE\Call51\Bin\project1.HEX
